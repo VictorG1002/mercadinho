53099844dbd80613e546cc4363ee0b8072daa879
Victor <vggaia10@gmail.com>
2022-08-06 18:11:33 -0300

    end header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,23 @@
 import React from 'react'
-import { Container } from './style'
+import { Link } from 'react-router-dom'
+import { Bar, Cart, Container } from './style'
+import { MdShoppingBasket } from 'react-icons/md'
 
 // import { Container } from './styles';
 
 function Header() {
   return (
     <Container>
-      <h1>Mercadinho Ponta Verde</h1>
+      <Link to="/">
+        <h1>Mercadinho PontaVerde</h1>
+      </Link>
+      <Cart to="/cart">
+        <div>
+          <strong>Meu carrinho</strong>
+          <span>3 itens</span>
+        </div>
+        <MdShoppingBasket size={36} color="#304d63" />
+      </Cart>
     </Container>
   )
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,3 +1,42 @@
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
-export const Container = styled.div``
+export const Container = styled.div`
+  background: #fff;
+  display: flex;
+  padding: 30px;
+  box-shadow: 2px 5px 5px RGBA(0, 0, 0, 0.24);
+  align-items: center;
+  justify-content: space-between;
+
+  a {
+    text-decoration: none;
+    color: #304d63;
+  }
+`
+
+export const Cart = styled(Link)`
+  display: flex;
+  align-items: center;
+  text-decoration: none;
+  transition: opacity 0.2;
+
+  &:hover {
+    opacity: 0.7;
+  }
+
+  div {
+    text-align: rigt;
+    margin-right: 10px;
+
+    strong {
+      display: block;
+      color: #304d63;
+    }
+
+    span {
+      font-size: 12px;
+      color: #898;
+    }
+  }
+`
