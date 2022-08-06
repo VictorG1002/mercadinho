c11169ca247d6659aa4a23bad044dcecfcb62dd5
Victor <vggaia10@gmail.com>
2022-08-06 13:23:02 -0300

    add Header to routes

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from './style'
+
+// import { Container } from './styles';
+
+function Header() {
+  return (
+    <Container>
+      <h1>Mercadinho Ponta Verde</h1>
+    </Container>
+  )
+}
+
+export default Header
