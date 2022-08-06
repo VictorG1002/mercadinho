a829ba9b4c768b59050dc0fd2078ed059b83b390
Victor <vggaia10@gmail.com>
2022-08-06 18:10:32 -0300

    add header to global router

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Header from '../components/Header'
 import Cart from '../pages/Cart'
 import Home from '../pages/Home'
 
@@ -8,6 +9,7 @@ import Home from '../pages/Home'
 function MyRoutes() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/cart" element={<Cart />} />
