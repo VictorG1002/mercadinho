4cf31b28033a37483b5c768f3cf0c498e7b93961
Victor <vggaia10@gmail.com>
2022-08-08 10:57:37 -0300

    add response api

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,84 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { ProductList } from './style'
-import { MdAddShoppingCart } from 'react-icons/md'
+import { MdAddShoppingCart, MdApi } from 'react-icons/md'
+import api from '../../services/api'
 
 // import { Container } from './styles';
 
 function Home() {
+  const [products, setProducts] = useState([])
+
+  const getResponse = async () => {
+    const response = await api.get('products')
+    setProducts(response.data)
+  }
+
+  useEffect(() => {
+    getResponse()
+  }, [])
   return (
     <ProductList>
-      <li>
-        <img src="https://saude.abril.com.br/wp-content/uploads/2021/04/alimentacao-fruta-feia-Betka82-Getty-Images.png" />
-        <strong>Fruta zarolha</strong>
-        <span>15,99</span>
+      {products.map(product => (
+        <li key={product.id}>
+          <img src="https://saude.abril.com.br/wp-content/uploads/2021/04/alimentacao-fruta-feia-Betka82-Getty-Images.png" />
+          <strong>{product.title}</strong>
+          <span>{`R$ ${product.price}`}</span>
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://saude.abril.com.br/wp-content/uploads/2021/04/alimentacao-fruta-feia-Betka82-Getty-Images.png" />
-        <strong>Fruta zarolha</strong>
-        <span>15,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://saude.abril.com.br/wp-content/uploads/2021/04/alimentacao-fruta-feia-Betka82-Getty-Images.png" />
-        <strong>Fruta zarolha</strong>
-        <span>15,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://saude.abril.com.br/wp-content/uploads/2021/04/alimentacao-fruta-feia-Betka82-Getty-Images.png" />
-        <strong>Fruta zarolha</strong>
-        <span>15,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://saude.abril.com.br/wp-content/uploads/2021/04/alimentacao-fruta-feia-Betka82-Getty-Images.png" />
-        <strong>Fruta zarolha</strong>
-        <span>15,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
-      <li>
-        <img src="https://saude.abril.com.br/wp-content/uploads/2021/04/alimentacao-fruta-feia-Betka82-Getty-Images.png" />
-        <strong>Fruta zarolha</strong>
-        <span>15,99</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#fff" /> 3
-          </div>
-          <span>Adicionar ao carrinho</span>
-        </button>
-      </li>
+          <button type="button">
+            <div>
+              <MdAddShoppingCart size={16} color="#fff" /> 3
+            </div>
+            <span>Adicionar ao carrinho</span>
+          </button>
+        </li>
+      ))}
     </ProductList>
   )
 }
