627a0e1566ad3c13a0a8d5771f7c246e7a628162
Victor <vggaia10@gmail.com>
2022-08-09 12:09:05 -0300

    create reducer

---
+++ src/store/modules/cart/reducer.js
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit'
+
+export const cartSlice = createSlice({
+  name: 'cart',
+  initialState: {
+    products: []
+  },
+  reducers: {
+    addToCart: (state, action) => {
+      const before = state.products
+      state.products = [...before, action.payload]
+    }
+  }
+})
+
+// Action creators are generated for each case reducer function
+export const { addToCart } = cartSlice.actions
+
+export default cartSlice.reducer
