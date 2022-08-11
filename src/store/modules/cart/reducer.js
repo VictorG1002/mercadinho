e2847fb21d1971c8d7b7134ddb072c221dcfeaa8
Victor <vggaia10@gmail.com>
2022-08-11 12:14:59 -0300

    add updateAmount Reducer

--- src/store/modules/cart/reducer.js
+++ src/store/modules/cart/reducer.js
@@ -7,19 +7,42 @@ export const cartSlice = createSlice({
   },
   reducers: {
     addToCart: (state, action) => {
-      const before = state.products
-      state.products = [
-        ...before,
-        {
-          ...action.payload,
-          amount: 1
-        }
-      ]
+      const productIndex = state.products.findIndex(
+        p => p.id == action.payload.id
+      )
+      if (productIndex >= 0) {
+        state.products[productIndex].amount += 1
+      } else {
+        const before = state.products
+        state.products = [
+          ...before,
+          {
+            ...action.payload,
+            amount: 1
+          }
+        ]
+      }
+    },
+    removeToCart: (state, action) => {
+      const productIndex = state.products.findIndex(
+        p => p.id == action.payload.id
+      )
+      if (productIndex >= 0) {
+        state.products.splice(productIndex, 1)
+      }
+    },
+    updateAmount: (state, action) => {
+      const productIndex = state.products.findIndex(
+        p => p.id == action.payload.id
+      )
+      if (productIndex >= 0) {
+        state.products[productIndex].amount = action.payload.amount
+      }
     }
   }
 })
 
 // Action creators are generated for each case reducer function
-export const { addToCart } = cartSlice.actions
+export const { addToCart, removeToCart, updateAmount } = cartSlice.actions
 
 export default cartSlice.reducer
